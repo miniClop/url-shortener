0d67fd143c9df74600a3d005e5c74f8a588bc362
Nikita Vokhmyanin <4dragooon4@gmail.com>
2023-07-15 15:17:03 +0300

    init config

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"url-shortener/internal/config"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	// TODO: init logger: slog
+
+	// TODO: init storage: sqlite
+
+	// TODO: init router: chi, "chi render"
+
+	// TODO: run server:
+}
